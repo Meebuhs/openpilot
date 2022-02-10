3123de1b38d0ed73eabc8219f11ddd5e63988766
Meebuhs <alexeksharpe@gmail.com>
2022-02-10 15:23:12 +1000

    Keep OP engaged when accelerator is pressed

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -32,6 +32,7 @@ class CarInterfaceBase(ABC):
     self.steering_unpressed = 0
     self.low_speed_alert = False
     self.silent_steer_warning = True
+    self.disengage_on_acc = False
 
     if CarState is not None:
       self.CS = CarState(CP)
@@ -125,7 +126,7 @@ class CarInterfaceBase(ABC):
       events.add(EventName.wrongCarMode)
     if cs_out.espDisabled:
       events.add(EventName.espDisabled)
-    if cs_out.gasPressed:
+    if cs_out.gasPressed and self.disengage_on_acc:
       events.add(EventName.gasPressed)
     if cs_out.stockFcw:
       events.add(EventName.stockFcw)
@@ -154,7 +155,7 @@ class CarInterfaceBase(ABC):
       events.add(EventName.steerUnavailable)
 
     # Disable on rising edge of gas or brake. Also disable on brake when speed > 0.
-    if (cs_out.gasPressed and not self.CS.out.gasPressed) or \
+    if (self.disengage_on_acc and (cs_out.gasPressed and not self.CS.out.gasPressed)) or \
        (cs_out.brakePressed and (not self.CS.out.brakePressed or not cs_out.standstill)):
       events.add(EventName.pedalPressed)
 
@@ -166,7 +167,6 @@ class CarInterfaceBase(ABC):
         events.add(EventName.pcmDisable)
 
     return events
-
 
 class RadarInterfaceBase(ABC):
   def __init__(self, CP):
