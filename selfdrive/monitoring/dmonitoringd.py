06a3859c54c4492e7ddae2513f14a7d259156794
Meebuhs <alexeksharpe@gmail.com>
2025-04-11 18:02:19 +1000

    small fixes

--- selfdrive/monitoring/dmonitoringd.py
+++ selfdrive/monitoring/dmonitoringd.py
@@ -16,28 +16,29 @@ def dmonitoringd_thread():
 
   # 20Hz <- dmonitoringmodeld
   while True:
-    sm.update()
-    if not sm.updated['driverStateV2']:
-      # iterate when model has new output
-      continue
+    return
+    # sm.update()
+    # if not sm.updated['driverStateV2']:
+    #   # iterate when model has new output
+    #   continue
 
-    valid = sm.all_checks()
-    if valid:
-      DM.run_step(sm)
+    # valid = sm.all_checks()
+    # if valid:
+    #   DM.run_step(sm)
 
-    # publish
-    dat = DM.get_state_packet(valid=valid)
-    pm.send('driverMonitoringState', dat)
+    # # publish
+    # dat = DM.get_state_packet(valid=valid)
+    # pm.send('driverMonitoringState', dat)
 
-    # load live always-on toggle
-    if sm['driverStateV2'].frameId % 40 == 1:
-      DM.always_on = params.get_bool("AlwaysOnDM")
+    # # load live always-on toggle
+    # if sm['driverStateV2'].frameId % 40 == 1:
+    #   DM.always_on = params.get_bool("AlwaysOnDM")
 
-    # save rhd virtual toggle every 5 mins
-    if (sm['driverStateV2'].frameId % 6000 == 0 and
-     DM.wheelpos_learner.filtered_stat.n > DM.settings._WHEELPOS_FILTER_MIN_COUNT and
-     DM.wheel_on_right == (DM.wheelpos_learner.filtered_stat.M > DM.settings._WHEELPOS_THRESHOLD)):
-      params.put_bool_nonblocking("IsRhdDetected", DM.wheel_on_right)
+    # # save rhd virtual toggle every 5 mins
+    # if (sm['driverStateV2'].frameId % 6000 == 0 and
+    #  DM.wheelpos_learner.filtered_stat.n > DM.settings._WHEELPOS_FILTER_MIN_COUNT and
+    #  DM.wheel_on_right == (DM.wheelpos_learner.filtered_stat.M > DM.settings._WHEELPOS_THRESHOLD)):
+    #   params.put_bool_nonblocking("IsRhdDetected", DM.wheel_on_right)
 
 def main():
   dmonitoringd_thread()
